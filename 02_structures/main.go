84b3ca96c68772bb6eb45ae26f2cc742280f6008
David Levy <lennox.davidlevy@gmail.com>
2022-04-13 23:04:51 -0400

    chapter 3

--- 02_structures/main.go
+++ 02_structures/main.go
@@ -2,17 +2,95 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	goku := Saiyan{}
+// 	goku.Name = "Goku"
+// 	goku.Power = 9000
+// 	fmt.Println(goku)
+// }
+
+// func Super(s *Saiyan) {
+// }
+
+// func main() {
+// 	goku := &Saiyan{"Goku", 9001}
+// 	goku.Super()
+// 	fmt.Println(goku.Power)
+// }
+
+// func main() {
+// 	goku := NewSaiyan("David", 18000)
+// 	goku.Super()
+// 	fmt.Println(goku)
+// }
+
+// type Saiyan struct {
+// 	Name   string
+// 	Power  int
+// 	Father *Saiyan
+// }
+
+// func (s *Saiyan) Super() {
+// 	s.Power += 10000
+// }
+
+type Person struct {
+	Name string
+}
+
+func (p *Person) Introduce() {
+	fmt.Printf("Hi, I'm %s\n", p.Name)
+}
+
 type Saiyan struct {
-	Name  string
+	*Person
 	Power int
 }
 
-func main() {
-	goku := &Saiyan{"Goku", 9000}
-	Super(goku)
-	fmt.Println(goku)
+//OverWrite
+func (s *Saiyan) Introduce() {
+	fmt.Printf("Hi, I'm %s. Ya!\n", s.Name)
 }
 
-func Super(s *Saiyan) {
-	s.Power += 10000
+func main() {
+	goku := &Saiyan{
+		Person: &Person{"Goku"},
+		Power:  9001,
+	}
+	goku.Introduce()
 }
+
+// func main() {
+// 	goku := &Saiyan{
+// 		Name:   "Goku",
+// 		Power:  9001,
+// 		Father: nil,
+// 	}
+// 	gohan := &Saiyan{
+// 		Name:   "Gohan",
+// 		Power:  1000,
+// 		Father: goku,
+// 	}
+// 	fmt.Println(gohan)
+// }
+
+// func main() {
+// 	goku := new(Saiyan)
+// 	goku.Name = "Joey"
+// 	goku.Power = 9000
+// 	goku.Super()
+// 	fmt.Println(goku)
+// }
+
+// func NewSaiyan(name string, power int) *Saiyan {
+// 	return &Saiyan{
+// 		Name:  name,
+// 		Power: power,
+// 	}
+// }
+// func NewSaiyan(name string, power int) Saiyan {
+// 	return Saiyan{
+// 		Name:  name,
+// 		Power: power,
+// 	}
+// }
